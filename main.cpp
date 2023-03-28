2d192c9db571882e1ac5b04df2307ccda014a38e
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-28 20:40:55 +0200

    Read meshes from a folder

--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <cstdio>
+#include <iostream>
+#include <filesystem>
 
 #include "indexed_mesh.hpp"
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        printf("Expected arguments: mesh.stl\n");
+        printf("Expected arguments: path/to/folder/full/of/STLs\n");
         return 0;
     }
-    char *filepath = argv[1];
-    auto mesh = read_indexed_mesh_from_stl(filepath);
+    char *folder_path = argv[1];
+
+    for (const auto &entry: std::filesystem::directory_iterator(folder_path)) {
+        auto path = entry.path();
+        if (path.extension() == ".stl") {
+            auto mesh = read_indexed_mesh_from_stl((const char *) path.string().c_str());
+        }
+    }
+
     return EXIT_SUCCESS;
 }
