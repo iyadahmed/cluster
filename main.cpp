8af25f8daa43d588d6e7db19779739a2f5448a79
Iyad Ahmed <iyadahmed430@gmail.com>
2023-03-29 03:41:54 +0200

    Support passing file path as program argument

--- main.cpp
+++ main.cpp
@@ -6,16 +6,20 @@
 
 int main(int argc, char **argv) {
     if (argc != 2) {
-        printf("Expected arguments: path/to/folder/full/of/STLs\n");
+        printf("Expected arguments: path/to/stl/or/directory/of/STLs\n");
         return 0;
     }
-    char *folder_path = argv[1];
+    char *input_path = argv[1];
 
-    for (const auto &entry: std::filesystem::directory_iterator(folder_path)) {
-        const std::filesystem::path &path = entry.path();
-        if (path.extension() == ".stl") {
-            auto mesh = read_indexed_mesh_from_stl((const char *) path.string().c_str());
+    if (std::filesystem::is_directory(input_path)) {
+        for (const auto &entry: std::filesystem::directory_iterator(input_path)) {
+            const std::filesystem::path &path = entry.path();
+            if (path.extension() == ".stl") {
+                auto mesh = read_indexed_mesh_from_stl((const char *) path.string().c_str());
+            }
         }
+    } else {
+        auto mesh = read_indexed_mesh_from_stl(input_path);
     }
 
     return EXIT_SUCCESS;
